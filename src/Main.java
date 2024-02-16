7c1af2855b6be726be6255613afe4d25475b75cd
Claudio Genco <142116218+Calyen93@users.noreply.github.com>
2024-02-16 14:14:31 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[]args){
         Auto car1 = new Auto(1300, " Fiat ", " Punto Evo ", " BL412EF ");
